1274ab265e26c4cd27f562285a040b73fd362159
Juan Martín <juanmarodribizon@gmail.com>
2026-08-09 00:04:09 -0300

    0.4: Modo oscuro (primera funcion en main.js) - Cambiado el color de fondo a un gris claro

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+// MODO OSCURO
+
+// 1. Buscamos el botón y el cuerpo de la web
+const botonmodo = document.getElementById('boton-modo');
+const cuerpo = document.body;
+
+// 2. Definimos qué hacer cuando se haga clic
+botonmodo.addEventListener('click', function() {
+
+    // toggle agrega la clase si no está, y la quita si ya está
+    cuerpo.classList.toggle('modo-oscuro');
+
+    // Comprobamos: ¿El body tiene ahora la clase 'dark-mode'?
+    if (cuerpo.classList.contains('modo-oscuro')) {
+    // Si la tiene, cambiamos el texto a "Modo Claro" con un sol
+    boton.textContent = "Modo Claro ☀️";
+    } else {
+        // Si NO la tiene, volvemos al texto original
+        boton.textContent = "Modo Oscuro 🌙";
+    }
+});
