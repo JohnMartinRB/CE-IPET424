322f342b94e68c47205c7bc83000598e767b781b
Juan Martín <juanmarodribizon@gmail.com>
2026-08-15 16:57:47 -0300

    0.8 Restructuración del index: ahora el header y el footer se encuentran en archivos individuales. Se creó una carpeta "components" con header.html y footer.html, los cuales se conectan al index con una nueva funcion para cargar componentes en main.js (usando la función FETCH) - Se reordenaron algunas líneas de styles.css  y main.js y se eliminaron algunas líneas vacías - Se unificaron Nunito y Nunito Sans en un mismo enlace a G. Fonts - Se eliminaron etiquetas meta obsoletas - Se agrego una descripcion en meta description -  Se eliminaron los link "preload" - Se cambió la ubicación del link a main.js pasando a estar en el head con el atributo DEFER

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
 
 
-// MODO OSCURO
+// Función para cargar HTML dinámicamente
+function cargarComponente(idContenedor, archivoHTML) {
+    fetch(archivoHTML)
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById(idContenedor).innerHTML = data;
+    });
+}
+// Cargar el header y el footer al abrir la página
+document.addEventListener("DOMContentLoaded", () => {
+    // Insertar "header.html" dentro de la etiqueta "header-container"
+    cargarComponente("header-container", "components/header.html");
+    cargarComponente("footer-container", "components/footer.html");
+});
 
+
+// MODO OSCURO
 // 1. Buscamos el botón y el cuerpo de la web
 const botonmodo = document.getElementById('boton-modo');
 const cuerpo = document.body;
-
 // 2. Definimos qué hacer cuando se haga clic
 botonmodo.addEventListener('click', function() {
-
     // toggle agrega la clase si no está, y la quita si ya está
     cuerpo.classList.toggle('modo-oscuro');
-
     // Comprobamos: ¿El body tiene ahora la clase 'dark-mode'?
     if (cuerpo.classList.contains('modo-oscuro')) {
     // Si la tiene, cambiamos el texto a "Modo Claro" con un sol
