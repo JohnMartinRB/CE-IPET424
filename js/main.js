5502eabbe7baddc3b940f57bd024401850e0abc6
Juan Martín Rodríguez Bizón <juanmarodribizon@gmail.com>
2026-08-17 18:14:49 -0300

    0.14.2 Se agregó un color oscuro para ::selection en dark mode - Se actualizó un easter egg y se agregó uno nuevo

--- js/main.js
+++ js/main.js
@@ -11,6 +11,21 @@ document.querySelectorAll('img').forEach(img => {
     img.addEventListener('contextmenu', e => e.preventDefault());
 });
 
+// Konami Code (Easter Egg)
+const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let konamiIndex = 0;
+document.addEventListener('keydown', (e) => {
+    if (e.key.toLowerCase() === konamiCode[konamiIndex].toLowerCase()) {
+        konamiIndex++;
+        if (konamiIndex === konamiCode.length) {
+        alert('🚀 ¡Descubriste el modo desarrollador del CE IPET 424!');
+        konamiIndex = 0;
+        }
+    } else {
+        konamiIndex = 0;
+    }
+});
+
 // Easter egg en la consola
 console.log(
     "%c ¡Hola dev! 🚀 %c\n¿Te interesa la programación o querés colaborar en la web del Centro de Estudiantes? ¡Sumate al equipo!",
