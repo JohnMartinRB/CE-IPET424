b0fa101483ec2b8bcf824ab4f5a02dcf2faa8434
Juan Martín Rodríguez Bizón <juanmarodribizon@gmail.com>
2026-09-02 23:54:49 -0300

    0.20.10 Se automatizó el numerado de versión del footer

--- js/components.js
+++ js/components.js
@@ -20,9 +20,25 @@ export async function initComponents() {
                 }
                 // SI SE CARGÓ EL FOOTER: Actualizamos el año automáticamente
                 if (idContenedor === "footer-container") {
+                    // 1. Año automático
                     const yearSpan = document.getElementById("copyright-year");
                     if (yearSpan) {
                         yearSpan.textContent = new Date().getFullYear();
+                    }
+                    // 2. Versión/Commit automático desde GitHub API
+                    const versionSpan = document.getElementById("version-tag");
+                    if (versionSpan) {
+                        fetch('https://api.github.com/repos/JohnMartinRB/CE-IPET424/commits/main')
+                            .then(res => res.json())
+                            .then(data => {
+                                // Toma el Hash corto (primeros 7 caracteres) Ej: 2f7515c
+                                const shortHash = data.sha.substring(0, 4); 
+                                versionSpan.textContent = `(0.20 #${shortHash})`;
+                            })
+                            .catch(() => {
+                                // Si falla por falta de internet o límite de API, mantiene el valor por defecto
+                                versionSpan.textContent = `0.20`;
+                            });
                     }
                 }
             })
